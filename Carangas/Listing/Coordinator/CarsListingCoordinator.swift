46d311d3c1a1ef7ea50dc267cafe1670d88883af
Eric Brito <eric_brito@msn.com>
2020-10-17 10:05:17 -0300

    - Primeiras implemntações de Coordinator

---
+++ Carangas/Listing/Coordinator/CarsListingCoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  CarsListingCoordinator.swift
+//  Carangas
+//
+//  Created by Eric Alves Brito on 17/10/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarsListingCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var parentCoordinator: Coordinator?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = CarsTableViewController.instantiateFromStoryboard(.listing)
+        viewController.coordinator = self
+        navigationController.pushViewController(viewController, animated: false)
+    }
+    
+    func showCarWith(viewModel: CarVisualizationViewModel) {
+        let viewController = CarViewController.instantiateFromStoryboard(.visualization)
+        viewController.viewModel = viewModel
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
